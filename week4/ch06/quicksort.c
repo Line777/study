0f616e72170bffccb093dc1e3fafde6f23903271
Line777 <58775401+Line777@users.noreply.github.com>
2021-10-24 19:00:48 +0800

    Create quicksort.c

---
+++ week4/ch06/quicksort.c
@@ -0,0 +1,35 @@
+void Swap(int arr[], int low, int high)
+{
+    int temp;
+    temp = arr[low];
+    arr[low] = arr[high];
+    arr[high] = temp;
+}
+ 
+int Partition(int arr[], int low, int high)
+{
+    int base = arr[low];
+    while(low < high)
+    {
+        while(low < high && arr[high] >= base)
+        {
+            high --;
+        }
+        Swap(arr, low, high);
+        while(low < high && arr[low] <= base)
+        {
+            low ++;
+        }
+        Swap(arr, low, high);
+    }
+    return low;
+}
+ 
+void QuickSort(int arr[], int low, int high)
+{
+    if(low < high)
+    {
+        int base = Partition(arr, low, high);
+        QuickSort(arr, low, base - 1);
+        QuickSort(arr, base + 1, high);
+    }
