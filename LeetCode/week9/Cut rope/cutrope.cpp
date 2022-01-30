cc39b5f2efc1944c4ff9bf698ca18c3e9efc84d3
Line777 <58775401+Line777@users.noreply.github.com>
2022-01-30 13:22:50 +0800

    Create cutrope.cpp

---
+++ LeetCode/week9/Cut rope/cutrope.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int cuttingRope(int n) {
+        if (n < 4) {
+            return n - 1;
+        }
+        vector<int> products(n + 1, 0);
+        products[1] = 1;
+        products[2] = 2;
+        products[3] = 3;
+        for (int i = 4; i <= n; ++i) {
+            int maxVal = 0;
+            for (int j = 1; j <= i / 2; ++j) {
+                maxVal = max(maxVal, products[j] * products[i - j]);
+            }
+            products[i] = maxVal;
+        }
+        return products[n];
+    }
+};
